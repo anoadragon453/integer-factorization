37c570c8bb0842a04e9ed07b4634a662006e5b6f
Andrew Morgan <andrew@amorgan.xyz>
2018-11-16 17:14:06 +0100

    Take in integer to factor from cmdline

--- factor.py
+++ factor.py
@@ -2,7 +2,18 @@
 
 import math
 import numpy
+import sys
+import os
 
+# Make sure the user provided a number to factor
+if len(sys.argv) is not 2:
+    print('Please input a single number to factor')
+    sys.exit(1)
+
+# Save number
+number = sys.argv[1]
+
+# Store first 1000 primes
 with open('1000_primes.txt') as primes:
     lines = [int(line.rstrip('\n')) for line in primes]
-    print (lines)
+
